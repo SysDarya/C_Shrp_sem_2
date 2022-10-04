e188c2ef525af39541ea85bdf2d0559138ca8f45
Sys Darya <sysdaryaandreevna@gmail.com>
2022-10-04 21:22:52 +0300

    task005

---
+++ 005/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая будет принимать на вход пять чисел и выводить сумму и среднее арифметическое этих чисел.
+
+Console.Write ("Введите первое число: ");
+int a = Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Введите второе число: ");
+int b = Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Введите третье число: ");
+int c = Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Введите четвертое число: ");
+int d = Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Введите пятое число: ");
+int e = Convert.ToInt32 (Console.ReadLine());
+
+int sum = a + b + c + d + e;
+double mid = sum/5;
+
+Console.WriteLine ($"Сумма чисел равна {sum}, а среднее арифметическое равно {mid}.");
